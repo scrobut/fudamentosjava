d5700235700d762e8d972727db252a8c64714628
Scrobut <diogoscrobut@hotmail.com>
2022-06-10 11:40:24 -0300

    fundamentos java atualização

---
+++ src/Classes/Carro.java
@@ -0,0 +1,17 @@
+package Classes;
+
+public class Carro {
+
+    String nome;
+    String marca;
+    int ano;
+    double velocidadeMax;
+
+
+    Carro(String nomeNew, String marcaNew, int anoNew, double velocidadeMaxNew){
+        nome = nomeNew;
+        marca = marcaNew;
+        ano = anoNew;
+        velocidadeMax = velocidadeMaxNew;
+    }
+}
---
+++ src/Classes/CarroTeste.java
@@ -0,0 +1,10 @@
+package Classes;
+
+public class CarroTeste {
+    public static void main(String[] args) {
+        Carro carrinhos = new Carro("ka", "ford",
+                2022, 180.80);{
+            System.out.println("o carro " + carrinhos.nome);
+        }
+    }
+}
